a8e328160990988dc7f0f5278b82517c7b97a8e2
Sean Mead <mead1sm@cmich.edu>
2014-12-10 19:45:49 -0500

    Using latest request

--- src/com/googleimage/MainFragment.java
+++ src/com/googleimage/MainFragment.java
@@ -1,13 +1,5 @@
 package com.googleimage;
 
-import java.util.List;
-
-import com.example.wolfram.R;
-import com.mead.request.Request;
-import com.mead.request.Response;
-import com.mead.request.content.Content;
-import com.mead.request.html.Div;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,15 +10,19 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.example.wolfram.R;
+import com.mead.request.Request;
+import com.mead.request.Response;
+import com.mead.request.content.Content;
+
 public class MainFragment extends Fragment implements Response, View.OnClickListener{
 	
 	private static final String SERVER = "https://www.google.com/search?&tbm=isch&q=";
 	
-	private ImageView mImageView;
-	private EditText mEditText;
-	private Button mButton;
+	private ImageView imageView;
+	private EditText editText;
 	private RelativeLayout rootView;
-	private Request mRequest;
+	private Request request;
 
 	public MainFragment() {}
 	
@@ -37,46 +33,30 @@ public class MainFragment extends Fragment implements Response, View.OnClickList
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		rootView = (RelativeLayout) inflater.inflate(R.layout.fragment_main, container, false);
-		mImageView = (ImageView) $(R.id.imageView);
-		mEditText = (EditText) $(R.id.editText);
-		mButton = (Button) $(R.id.button);
-		mButton.setOnClickListener(this);
-		mRequest = new Request(getActivity(), this);
+		imageView = (ImageView) $(R.id.imageView);
+		editText = (EditText) $(R.id.editText);
+		((Button) $(R.id.button)).setOnClickListener(this);
+		request = new Request(getActivity(), this);
 		return rootView;
 	}
 
 	@Override
-	public void onDecode(Content arg0) {
-		mImageView.setImageBitmap(arg0.Bitmap);
+	public void onDecode(Content content) {
+		imageView.setImageBitmap(content.Bitmap);
 	}
 
 	@Override
-	public void onGet(Content arg0) {
-		try{
-			List<String> images = arg0.parser.getImgs();
-			if(images.size() > 0){
-				String img = images.get(0);
-				String src = img.substring(img.indexOf("src=") + 5);
-				mRequest.decode(src.substring(0, src.indexOf("\"")));
-			}
-		}catch(Exception e){}
-		
-		
+	public void onGet(Content content) {
+		System.out.println(content.Type);
+		content.html();
 	}
 
 	@Override
-	public void onLogin(Content arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onPost(Content arg0) {
-		
-	}
+	public void onPost(Content content) {}
 
 	@Override
 	public void onClick(View v) {
-		mRequest.get(SERVER + mEditText.getText().toString());
+		request.get(SERVER + editText.getText().toString());
 	}
+
 }
